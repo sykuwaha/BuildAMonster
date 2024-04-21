f76497b2d306a2cee34e1634268e73ce081ffee8
sykuwaha <97131604+sykuwaha@users.noreply.github.com>
2024-04-21 11:45:02 -0700

    edit

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -23,23 +23,59 @@ class Monster extends Phaser.Scene {
     }
 
     create() {
-        let my = this.my;   // create an alias to this.my for readability
-
-        // Create the main body sprite
-        //
-        // this.add.sprite(x,y, "{atlas key name}", "{name of sprite within atlas}")
-        //
-        // look in spritesheet_default.xml for the individual sprite names
-        // You can also download the asset pack and look in the PNG/default folder.
+        let my = this.my; 
+    
         my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
 
-        
-    }
+        my.sprite.leg1 = this.add.sprite(this.bodyX + 30, this.bodyY + 100, "monsterParts", "leg_greenD.png");
+        my.sprite.leg2 = this.add.sprite(this.bodyX - 30, this.bodyY + 100, "monsterParts", "leg_greenD.png");
 
+        my.sprite.arm1 = this.add.sprite(this.bodyX + 80, this.bodyY + 10, "monsterParts", "arm_greenD.png");
+        my.sprite.arm2 = this.add.sprite(this.bodyX - 60, this.bodyY + 10, "monsterParts", "arm_greenD.png");
+        my.sprite.arm2.flipX = true;
+    
+        my.sprite.eye1 = this.add.sprite(this.bodyX - 50, this.bodyY - 60, "monsterParts", "eye_green.png");
+        my.sprite.eye1.flipX = true;
+        my.sprite.eye2 = this.add.sprite(this.bodyX + 50, this.bodyY - 60, "monsterParts", "eye_green.png");
+
+        my.sprite.eyething1 = this.add.sprite(this.bodyX - 50, this.bodyY - 60, "monsterParts", "eye_human_red.png");
+        my.sprite.eyething2 = this.add.sprite(this.bodyX + 50, this.bodyY - 60, "monsterParts", "eye_human_red.png");
+    
+        my.sprite.smile = this.add.sprite(this.bodyX, this.bodyY + 40, "monsterParts", "mouthF.png");
+        my.sprite.fangs = this.add.sprite(this.bodyX, this.bodyY + 40, "monsterParts", "mouth_closed_fangs.png");
+        my.sprite.fangs.setVisible(false);
+
+        my.sprite.accessory1 = this.add.sprite(this.bodyX - 35, this.bodyY - 120, "monsterParts", "nose_green.png");
+        my.sprite.accessory2 = this.add.sprite(this.bodyX + 35, this.bodyY - 120, "monsterParts", "nose_green.png");
+    }
+    
     update() {
-        let my = this.my;    // create an alias to this.my for readability
-
-       
+        let my = this.my;    
+    
+        if (Phaser.Input.Keyboard.JustDown(this.input.keyboard.addKey('S'))) {
+            my.sprite.smile.setVisible(true);
+            my.sprite.fangs.setVisible(false);
+        }
+    
+        if (Phaser.Input.Keyboard.JustDown(this.input.keyboard.addKey('F'))) {
+            my.sprite.smile.setVisible(false);
+            my.sprite.fangs.setVisible(true);
+        }
+    
+        if (this.input.keyboard.checkDown(this.input.keyboard.addKey('A'), 1)) {
+            for (let part in my.sprite) {
+                my.sprite[part].x -= 1;
+            }
+        }
+    
+        if (this.input.keyboard.checkDown(this.input.keyboard.addKey('D'), 1)) {
+            for (let part in my.sprite) {
+                my.sprite[part].x += 1;
+            }
+        }
     }
+    
+    
+    
 
 }
